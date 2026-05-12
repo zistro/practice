a7dac606ffd2d2849b1b88f8eecab5336e9c3116
Galib Chowdhury <zistro@proton.me>
2026-05-12 15:43:39 +0600

    Added files

--- string-prac.c
+++ string-prac.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-	char str1[50] = "Modon";
+	char str1[50] = "Modon Mohan";
 	char str2[50];
-	int i;
-	for (i=0; i!='\0'; i++) {
+	int i = 0;
+	while (str1[i] != '\0') {
 		str2[i] = str1[i];
+		i++;
 	}
 	str2[i] = '\0';
 	printf("%s", str2);
